571435c5cac1efd071f303942e5986fd2896e4ad
Robert <rgitonga.07@gmail.com>
2022-03-13 22:21:21 +0300

    st: add toggle effect on development column in what we do section

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+//Toggle image and text in what we do section
+
 $(document).ready(function(){
     $("#design-img").click(function(event){
         event.preventDefault();
@@ -7,4 +9,22 @@ $(document).ready(function(){
             $("#design-img, #design").toggle();
         });
     });
+    $("#develop-img").click(function(){
+        $("#development, #develop-img").toggle();
+        $("#development").click(function(){
+            $("#develop-img").toggle();
+            $("#development").toggle();
+        })
+    });
 });
+
+//Toggle image and text in development column of the what we do section
+
+// $(document).ready(function(){
+//     $("#develop-img").click(function(){
+//         $("#development, #develop-img").toggle();
+//         $("#development").click(function(){
+//             $("develop-img, #development").toggle();
+//         });
+//     });
+// });
